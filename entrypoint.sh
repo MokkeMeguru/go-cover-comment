bd64d121ee83fbc7b89288a76406e0382dcdea14
MokkeMeguru <meguru.mokke@gmail.com>
2021-05-31 23:35:33 +0900

    fix test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-exec 1> >(
-  while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
-    | tee -a $LOG_OUT
-)
+# exec 1> >(
+#   while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
+#     | tee -a $LOG_OUT
+# )
 
 echo "Welcome to Go Coverage Commenter"
 
