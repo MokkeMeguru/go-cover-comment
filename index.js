fdd9569868722ed4fb82f78b927dee08beb0efaa
MokkeMeguru <meguru.mokke@gmail.com>
2021-06-01 05:01:55 +0900

    unknown

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+
+function run() {
+    return new Promise(async (resolve, reject) => {
+        const repository = core.getInput("repository")
+        const head_ref = core.getInput("head_ref")
+        const base_ref = core.getInput("head_ref")
+        const exclude_regrexes = core.getInput("exclude-regrexes")
+        let result = 1;
+        try {
+            result = await exec("/app/scripts/entrypoint.sh", [repository, head_ref, base_ref, exclude_regrexes]);
+            if (result !== 0) {
+                throw new Error(`unknown error ${result}`);
+            }
+        } catch (err) {
+            error(err.message);
+        }
+    })
+}
+
+run();
