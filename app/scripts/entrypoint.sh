8fdb3a8cc44019d13be069357f9102193bbd95e3
MokkeMeguru <meguru.mokke@gmail.com>
2021-06-01 02:08:33 +0900

    remove exec

--- app/scripts/entrypoint.sh
+++ app/scripts/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -l
 
-exec 1> >(
-  while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
-    | tee -a $LOG_OUT
-)
-
 echo "Welcome to Go Coverage Commenter"
 
 # sh ./coverage.sh
